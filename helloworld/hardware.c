ed3ad10217c1bf132f775ddaa26b423eb61dc903
Thomas Reidemeister <treideme@gmail.com>
2019-10-22 21:24:19 -0400

    Baud rate generation

--- helloworld/hardware.c
+++ helloworld/hardware.c
@@ -9,6 +9,29 @@ void hardware_init() {
     // Power control
     PCON = INIT_PCON;
 
+    // Timer control
+    TCON = INIT_TCON;
+    TMOD = INIT_TMOD;
+
+    // Initialise timer registers for baud rate generation
+    // T2_overflow = 0xFFFC
+    // baud rate
+    //        Fclk = 12000000
+    // of = -------------------
+    //       32* [0xFFFF - T2]
+    // 125000.0 ~ 115200
+    RCAP2L = 0xFC;     //Initial timer value
+    RCAP2H = 0xFF;     //Initial timer value
+    //
+    T2CON = INIT_T2CON;
+
+
+    // Serial Control
+    SCON = INIT_SCON;
+
+    // Enable on-chip auxiliary RAM
+    AUXR = INIT_AUXR;
+
     // Configure watchdog
     WDT_CONTR = INIT_WDT_CONTR;
 
--- helloworld/hardware.h
+++ helloworld/hardware.h
@@ -43,6 +43,112 @@
   @endverbatim
 */
 
+#define INIT_SCON \
+  B8(01010000) /**< SCON  Serial Control. @verbatim
+     00000000 = reset
+     ||||||||
+     |||||||+-- (RW)  RI   : Receive Interrupt Flag
+     ||||||+--- (RW)  TI   : Transmit Interrupt Flag
+     |||||+---- (RO)  RB8  : 9-th received bit in 9-bit mode
+     ||||+----- (RW)  TB8  : 9-th transmitted bit in 9-bit mode
+     |||+------ (RW)  REN  : Enable receiver
+     ||+------- (RW)  SM2  : Serial Port Mode Bit 2 (Auto Address recognition)
+     |+-------- (RW)  SM1  : Serial Port Mode Bit 1
+     +--------- (RW)  SM0  : Serial Port Mode Bit 0
+
+  | SM0  | SM1  | Bandwith | Baud Source
+  |------|------|----------|------------
+  |  0   |  0   | 8-bit    | SysCLK/12
+  |  0   |  1   | 8-bit    | Variable
+  |  1   |  0   | 9-bit    | SysCLK/64 or SysCLK/32 (SMOD)
+  |  1   |  1   | 9-bit    | Variable
+
+  @endverbatim
+*/
+
+/******************************************************************************
+ * Timers
+ */
+
+#define INIT_TCON \
+  B8(00000000) /**< TCON  Timer Control. @verbatim
+     00000000 = reset
+     ||||||||
+     |||||||+-- (RW)  IT0 : Interrupt 0 type control bit (Low level)
+     ||||||+--- (RO)  IE0 : Interrupt 0 edge flag
+     |||||+---- (RO)  IT1 : Interrupt 1 type control bit (Low level)
+     ||||+----- (RW)  IE1 : Interrupt 1 edge flag
+     |||+------ (RW)  TR0 : Timer run 0 control bit (off)
+     ||+------- (RO)  TF0 : Timer 0 overflow flag
+     |+-------- (RW)  TR1 : Timer run 1 control bit (off)
+     +--------- (RO)  TF1 : Timer 1 overflow flag
+
+  @endverbatim
+*/
+
+#define INIT_TMOD \
+  B8(00000000) /**< TMOD  Timer Mode. @verbatim
+     00000000 = reset
+     ||||||||
+     |||||||+-- (RW)  M0   : Timer 0 Mode bit 0
+     ||||||+--- (RW)  M1   : Timer 0 Mode bit 1
+     |||||+---- (RW)  C/~T : Timer 0 Timer or counter (timer)
+     ||||+----- (RW)  GATE : Timer 0 Timer enabled whenever TR bit is set
+     |||+------ (RW)  M0   : Timer 1 Mode bit 0
+     ||+------- (RW)  M1   : Timer 1 Mode bit 1
+     |+-------- (RW)  C/~T : Timer 1 Timer or counter (timer)
+     +--------- (RW)  GATE : Timer 1 Timer enabled whenever TR bit is set
+
+  | M0 | M1 | Operating Mode
+  |----|----|---------------
+  | 0  | 0  | 8-bit Timer/Counter "THx" with "TLx" as 5-bit prescaler.
+  | 0  | 1  | 16-bit Timer/Counter"THx"and"TLx"are cascaded;there is no prescaler
+  | 1  | 0  | 8-bit auto-reload Timer/Counter “THx” holds a value which is to be reloaded into “TLx” each time it overflows
+  | 1  | 1  | (T0) TL0 is an 8-bit Timer/Counter controlled by the standard Timer 0 control bits TH0 is an 8-bit timer only controlled by Timer 1 control bits. (T1) is stopped
+
+  @endverbatim
+*/
+
+#define INIT_T2CON \
+  B8(00110100) /**< T2CON  Timer 2 Control. @verbatim
+     00000000 = reset
+     ||||||||
+     |||||||+-- (RW)  CP/~RL2: Auto reload
+     ||||||+--- (RW)  C/~T2  : Timer 2 as internal timer
+     |||||+---- (RW)  TR2    : Timer 2 run bit
+     ||||+----- (RW)  EXEN2  : External enable flag
+     |||+------ (RW)  TCLK   : Transmit clock flag (baud rate generation)
+     ||+------- (RW)  RCLK   : Receive clock flag (baud rate generation)
+     |+-------- (RW)  EXF2   : Timer 2 external flag
+     +--------- (RW)  TF2    : Timer 2 overflow flag
+
+  @endverbatim
+*/
+
+#define INIT_T2MOD \
+  B8(00000000) /**< T2CON  Timer 2 Control. @verbatim
+     00000000 = reset
+     xxxxxx||
+           |+-- (RW)  DCEN   : Down count enable bit
+           +--- (RW)  T2OE   : Timer 2 output enable bit
+
+  @endverbatim
+*/
+
+/*******************************************************************************
+ * Auxialiary RAM
+ */
+
+#define INIT_AUXR \
+  B8(00000001) /**< AUXR  Auxiliary Register 0. @verbatim
+     00000000 = reset
+           ||
+           |+-- (RW)  ALEOFF : Ale only active during MOVX, MOVC
+           +--- (RW)  EXTRAM : On-chip RAM enabled at 00H - FFH
+
+  @endverbatim
+*/
+
 #define INIT_WDT_CONTR \
   B8(00000000) /**< WDT_CONTR  Watchdog control register. @verbatim
      00000000 = reset
