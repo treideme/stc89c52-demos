790128fcd9d3186d0ca1d255765fc0d870306e72
Thomas Reidemeister <treideme@gmail.com>
2025-11-25 23:08:08 -0500

    Ethernet WiP

---
+++ 09_ethernet/enc28j60_cfg.h
@@ -0,0 +1,26 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file enc28j60_cfg.h Interface definition for driver code.
+ * @author Thomas Reidemeister
+ */
+#ifndef ENC28J60_CFG_H
+#define ENC28J60_CFG_H
+
+#define ENC28J60_CONTROL_CS P0_1
+#define ENC28J60_SPI_SCK P0_2
+#define ENC28J60_SPI_MISO P0_3
+#define ENC28J60_SPI_MOSI P0_4
+
+
+#endif // ENC28J60_CFG_H
